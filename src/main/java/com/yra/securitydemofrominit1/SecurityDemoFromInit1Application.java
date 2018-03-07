04b6eb34a5316e6266e2bd8b6bbb765e2f8b06c8
roman.yarosh <yracompany@gmail.com>
2018-03-07 18:16:41 +0200

    First

--- src/main/java/com/yra/securitydemofrominit1/SecurityDemoFromInit1Application.java
+++ src/main/java/com/yra/securitydemofrominit1/SecurityDemoFromInit1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityDemoFromInit1Application {
 
+	//https://o7planning.org/en/11705/create-a-login-application-with-spring-boot-spring-security-jpa
+
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityDemoFromInit1Application.class, args);
 	}
--- src/main/java/com/yra/securitydemofrominit1/config/WebSecurityConfig.java
+++ src/main/java/com/yra/securitydemofrominit1/config/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.activation.DataSource;
 
@@ -36,6 +38,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+
+        // The pages does not require login
+        http.authorizeRequests().antMatchers("/","/login","/logout").permitAll();
+
+        // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
+        // If no login, it will redirect to /login page.
+        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER','USER_ADMIN')");
+
+        // For ADMIN only.
+        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+
+        // When the user has logged in as XX.
+        // But access a page that requires role YY,
+        // AccessDeniedException will be thrown.
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+        // Config for Login Form
+        http.authorizeRequests().and().formLogin()
+        // Submit URL of login page.
+        .loginProcessingUrl("/j_spring_security_check") //Submit Url
+        .loginPage("/login")
+        .defaultSuccessUrl("/userAccountInfo")
+        .failureUrl("/login?error=true")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        // Config for Logout Page
+        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+
+        // Config Remember Me.
+        http.authorizeRequests().and()
+        .rememberMe().tokenRepository(this.persistentTokenRepository())
+        .tokenValiditySeconds(1*24*60*60);//24h
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+        db.setDataSource(dataSource);
+        return db;
     }
 }
