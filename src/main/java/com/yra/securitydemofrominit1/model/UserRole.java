e5f80ade3e546abda52073018aa237f798c57850
roman.yarosh <yracompany@gmail.com>
2018-03-07 18:25:26 +0200

    First

---
+++ src/main/java/com/yra/securitydemofrominit1/model/UserRole.java
@@ -0,0 +1,13 @@
+package com.yra.securitydemofrominit1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by YaroshR on 07.03.2018.
+ */
+@Entity
+@Table
+
+public class UserRole {
+}
